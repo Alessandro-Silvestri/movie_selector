5078014cf0fd5cceff69eac9f88f5b5ce42cd5ae
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2025-01-15 19:15:51 +0000

    insert first files with pseudo code

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+MOVIE SELECTOR PROJECT 
+
+*   navbar with genre
+*   carousel movies (possibly random images)
+*   genre selector
+*   dark mode option
+*   subscribe
+        (modal component with form)
+*   movie cards
+        (photo, title, genre, description)
+*   accordion with 5 news about cinema (possibly dynamic with API)
+*   footer
+
+*/
