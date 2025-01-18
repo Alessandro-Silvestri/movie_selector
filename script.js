a1204ec46277959fb62514fadb4e625a55015a52
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2025-01-18 20:26:25 +0000

    Dark mode added

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 /*
 MOVIE SELECTOR PROJECT 
 
-*   navbar with genre
 *   carousel movies (possibly random images)
 *   genre selector
 *   dark mode option
@@ -16,35 +15,47 @@ MOVIE SELECTOR PROJECT
 
 // all the cards
 let cards = document.getElementsByClassName('cardSelection');
-// console.log(cards[0].attributes.genre.value);
-
-
-
 
 // radio buttons
 let radioButtonsList = document.getElementsByName('btnRadio');
 for (let radio of radioButtonsList) {
         radio.addEventListener('change', (event) => {
                 let radioChoice = event.target.id;
-                for (let card of cards){
-                        if (radioChoice === 'all'){
-                                for (let card_ of cards){
+                for (let card of cards) {
+                        if (radioChoice === 'all') {
+                                for (let card_ of cards) {
                                         card_.parentElement.style.display = '';
                                 }
                         }
-                        else if (radioChoice === card.attributes.genre.value){
+                        else if (radioChoice === card.attributes.genre.value) {
                                 card.parentElement.style.display = '';
                         }
                         else {
-                                card.parentElement.style.display = 'none'; 
+                                card.parentElement.style.display = 'none';
                         }
                 }
 
         })
 }
 
+// dark mode
+let darkMode = document.getElementById('darkMode');
+let bodyTag = document.getElementsByTagName('body');
+let radioButtonsNames = document.getElementsByClassName('btn-outline-primary');
+let footer = document.getElementsByTagName('footer');
 
-
-// card4.parentElement.style.display = 'none';
-// card4.parentElement.style.display = '';
-
+darkMode.addEventListener('click', () => {
+        // card green, text white
+        bodyTag[0].classList.toggle('bg-dark');
+        for (let card of cards) {
+                card.children[1].classList.toggle('bg-success');
+                card.children[1].classList.toggle('text-light');
+        }
+        // radio buttons green
+        for (let radio of radioButtonsNames) {
+                radio.classList.toggle('btn-outline-success');
+        }
+        // border top to the footer
+        footer[0].classList.toggle('border-top');
+        footer[0].classList.toggle('border-success-subtle');
+})
